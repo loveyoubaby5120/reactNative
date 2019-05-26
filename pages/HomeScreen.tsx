8b436fd0b36930e0f430ed7f785dd54ce53971dc
pankai <273377900@qq.com>
2019-05-26 22:34:53 +0800

    TS

---
+++ pages/HomeScreen.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import { Mybutton } from './components/Mybutton';
+import { Mytext } from './components/Mytext';
+import { openDatabase, SQLiteDatabase } from 'react-native-sqlite-storage';
+const db: any = openDatabase({ name: 'UserDatabase.db', location: 'default' });
+
+export class HomeScreen extends React.Component<any, any> {
+    constructor(props) {
+        super(props);
+        db.transaction((txn) => {
+            txn.executeSql(
+                "SELECT name FROM sqlite_master WHERE type='table' AND name='table_user'",
+                [],
+                (tx, res) => {
+                    console.log('item:', res.rows.length);
+                    if (res.rows.length == 0) {
+                        txn.executeSql('DROP TABLE IF EXISTS table_user', []);
+                        txn.executeSql(
+                            'CREATE TABLE IF NOT EXISTS table_user(user_id INTEGER PRIMARY KEY AUTOINCREMENT, user_name VARCHAR(20), user_contact INT(10), user_address VARCHAR(255))',
+                            []
+                        );
+                    }
+                }
+            );
+        });
+    }
+
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    backgroundColor: 'white',
+                    flexDirection: 'column',
+                }}>
+                <Mytext text="SQLite Example" />
+                <Mybutton
+                    title="Register"
+                    customClick={() => this.props.navigation.navigate('Register')}
+                />
+                <Mybutton
+                    title="Update"
+                    customClick={() => this.props.navigation.navigate('Update')}
+                />
+                <Mybutton
+                    title="View"
+                    customClick={() => this.props.navigation.navigate('View')}
+                />
+                <Mybutton
+                    title="View All"
+                    customClick={() => this.props.navigation.navigate('ViewAll')}
+                />
+                <Mybutton
+                    title="Delete"
+                    customClick={() => this.props.navigation.navigate('Delete')}
+                />
+            </View>
+        );
+    }
+}
